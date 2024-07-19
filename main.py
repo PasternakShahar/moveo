6702466db0be047096f3becd1616400e14fc08a5
PasternakShahar <131560123+PasternakShahar@users.noreply.github.com>
2024-07-19 13:21:59 +0300

    add comments main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit
 
 
-
+# Create Flask app and set secret key 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Set your secret key
-socketio = SocketIO(app)
+socketio = SocketIO(app)         # Create a SocketIO object
 
-code_blocks = {
+code_blocks = {                   #code blocks -"list of at least 4 items which represents code blocks"
     "PrintExample": {
         "title": "Print Example",
         "code": "function printData() {\n    console.log('Hello, World!');\n}"
@@ -26,30 +26,32 @@ code_blocks = {
     }
 }
 
-# sTrack the first user for each code block
+# Track the first user for each code block - "Both users should enter this page. (2 different clients)
+Assume that the first user who opens the code block page is the mentor, after that, any other user will be counted as a student."
+
 mentors = {}
 
-#@app.route("/")
+#@app.route("/")              
 #def index():s
 #    return render_template("index.html")
 
-@app.route("/")
-def home():
+@app.route("/")                  #The function home() is defined to handle requests to the root URL.
+def home():                     
     return render_template('index.html')
 
-@app.route('/code/<block_name>')
+@app.route('/code/<block_name>')       #The functionis defined to handle User choices requests 
 def code_block(block_name):
     requested_block = code_blocks.get(block_name)
 
-    if block_name not in mentors:
+    if block_name not in mentors:          ## Determine the user role as mentor or student
         mentors[block_name] = session['user_id'] = request.remote_addr  # Assign mentor role to first user
 
     user_role = 'mentor' if session.get('user_id') == mentors[block_name] else 'student'
 
-    print(f"Requested block: {block_name}")
+    print(f"Requested block: {block_name}")  # Not necessary but makes it easier to know what state the user is in 
     print(f"Client IP: {request.remote_addr}, Role: {user_role}")
 
-    if requested_block:
+    if requested_block:   #Handling Missing Code Block:
         return render_template('code_block.html', title=requested_block['title'], code=requested_block['code'], user_role=user_role)
     else:
         return "Code block not found", 404
@@ -66,6 +68,6 @@ def handle_update_code(updated_code):
     print(f"Updating code: {updated_code}")  # Log the updated code
     emit('code_update', updated_code, broadcast=True)  # Send updated code to all clients
 
-if __name__ == "__main__":
+if __name__ == "__main__":        #check ensures that certain code is only executed when the script is run directly, not when it is imported.
     socketio.run(app, debug=True)
-    app.run(port=5000)
+    app.run(port=5000)           # port for app 
